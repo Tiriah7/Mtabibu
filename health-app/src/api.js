414c37fadc55f424b61723460adca30d5763e2fa
Tiriah7 <84663205+Tiriah7@users.noreply.github.com>
2026-03-19 21:45:31 +0300

    Update api.js

--- health-app/src/api.js
+++ health-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // use env variable for production
+  baseURL: 'https://mtabibu-backend.onrender.com', 
 });
 
 export default API;
