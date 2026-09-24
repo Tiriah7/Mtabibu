2859c823ead103f99a6096cac395a120363994c8
Tiriah7 <ndunguchris49@gmail.com>
2026-09-24 13:22:29 +0300

    Update on db credentials

--- health-system/config/db.js
+++ health-system/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Connect to database
 const connectDB = async() => {
 
-    const uri = process.env.MONGO_URI || 'mongodb+srv://reece:test1234@mutalldata.9qn3j.mongodb.net/health-system?retryWrites=true&w=majority&appName=mutalldata';
+    const uri = process.env.MONGO_URI || 'mongodb+srv://reece:IR2poL5f34FxNW3O@Health-app.bpszb4x.mongodb.net/health-system?retryWrites=true&w=majority&appName=Health-app';
     mongoose.connect(uri)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
