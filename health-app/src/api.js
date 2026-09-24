a1b83cb4f90f3c3424c768e583cf73eba67c9622
Tiriah7 <ndunguchris49@gmail.com>
2026-09-24 13:29:43 +0300

    Update on db credentials

--- health-app/src/api.js
+++ health-app/src/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-<<<<<<< HEAD
+
   baseURL: 'https://mtabibu-backend.onrender.com/', 
-=======
-  baseURL: 'https://mtabibu-backend.onrender.com', 
->>>>>>> 414c37fadc55f424b61723460adca30d5763e2fa
+
 });
 
 export default API;
